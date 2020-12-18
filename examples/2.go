90a3cad43ced17c0a1015f6aa7effef1677e4ba1
chyroc <chyroc@qq.com>
2020-12-18 13:28:34 +0800

    examples: spinner

--- examples/2.go
+++ examples/2.go
@@ -2,39 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"os"
-	"strings"
-	"sync"
 	"time"
 
-	"github.com/chyroc/tui/internal"
+	"github.com/muesli/termenv"
 )
 
-var i int
-
-func randInput() string {
-	i++
-	rand.Seed(time.Now().UnixNano())
-	s := fmt.Sprintf("= %d =\n", i)
-	for i := 0; i < int(rand.Int31n(10)); i++ {
-		if i > 0 {
-			s += "\n"
-		}
-
-		s += strings.Repeat("x", int(rand.Int31n(20)))
-	}
-	return s
-}
-
 func main() {
-	r := internal.NewRenderer(os.Stdout, new(sync.Mutex))
-
-	r.Start()
-	defer r.Stop()
-
-	for {
-		r.Write(randInput())
-		time.Sleep(time.Second)
-	}
+	fmt.Print(termenv.CSI + "5m")
+	time.Sleep(time.Hour)
 }
---
+++ examples/spinner/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/tj/go-terminput"
+
+	"github.com/chyroc/tui"
+)
+
+type worker struct {
+	tui      tui.TUI
+	count    int
+	options  [][]string
+	selected int
+}
+
+func (r *worker) Init() error {
+	go func() {
+		for {
+			r.count++
+			time.Sleep(time.Second / 10) // 一秒10变
+		}
+	}()
+	return nil
+}
+
+func (r *worker) Close() error {
+	return nil
+}
+
+func (r *worker) View() string {
+	count := r.count
+	options := r.options
+	selected := r.selected
+
+	// fmt.Println("view", count, selected, options[selected][count%len(options[selected])])
+	return options[selected][count%len(options[selected])]
+}
+
+func (r *worker) HandleInput(e *terminput.KeyboardInput) {
+	switch {
+	case e.Key() == terminput.KeyEscape || e.Rune() == 'q' || e.Key() == tui.KeyCtrlC:
+		tui.Stop(r.tui)
+	case e.Key() == terminput.KeyUp || e.Key() == terminput.KeyLeft:
+		if r.selected > 0 {
+			r.selected--
+		} else {
+			r.selected = len(r.options) - 1
+		}
+	case e.Key() == terminput.KeyDown || e.Key() == terminput.KeyRight:
+		if r.selected < len(r.options)-1 {
+			r.selected++
+		} else {
+			r.selected = 0
+		}
+	default:
+		fmt.Printf("e=%s, e.ctrl=%v, rune=%v, mod=%v, key=%v\n", e, e.Ctrl(), e.Rune(), e.Mod(), e.Key())
+	}
+}
+
+func NewWorker() *worker {
+	return &worker{}
+}
+
+func main() {
+	worker := NewWorker()
+	r := tui.New(worker)
+	worker.tui = r
+	worker.options = [][]string{
+		{"🌎", "🌍", "🌏"},
+		{"👆", "👉", "👇", "👈"},
+		{"⚪️", "⚫️", "🔴", "🔵"},
+		{"💛", "💚", "💙", "💜", "🖤"},
+		{"☀️", "⛅️", "🌦", "☁️", "🌧", "⛈", "🌩"},
+		{"🌕", "🌖", "🌗", "🌘", "🌑", "🌒", "🌓", "🌔"},
+		{"🚗", "🚕", "🚙", "🚌", "🏎", "🚓", "🚑", "🚒"},
+		{"0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"},
+		{"🕜", "🕝", "🕞", "🕟", "🕠", "🕡", "🕢", "🕣", "🕤", "🕥", "🕦", "🕧"},
+	}
+
+	if err := r.Run(); err != nil {
+		panic(err)
+	}
+}
